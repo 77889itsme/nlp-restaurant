f790ca07c0d1d9aca955b3844a4f8a2b6ae31958
your name goes here <133010759+77889itsme@users.noreply.github.com>
2024-12-08 18:52:46 -0500

    update file structure

---
+++ Main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import os
+import requests
+from sentiment_analysis.app import run_sentiment_analysis
+from recommendation.app import run_recommendation
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+@st.cache_data
+def read_pickle(file_path):
+    import pickle
+    dat = pickle.load(open(file_path, "rb"))
+    df = pd.DataFrame(dat)
+    return df
+
+df = read_pickle("data/dat.pk")
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+# ++++++++++ below are streamlit code snippets +++++++++++  #
+st.title("Taste & Text")
+st.sidebar.title("Navigation")
+
+module = st.sidebar.radio("Go to", ["🏠 Home", "📊 Sentiment Analysis", "🍴 Recommendation"])
+
+
+if module == "📊 Sentiment Analysis":
+    run_sentiment_analysis(df) 
+elif module == "🍴 Recommendation":
+    run_recommendation(df) 
+else:
+    st.header("Analysis of Restaurant Reviews in California")
+    st.write("Welcome to our group project's interactive app! Select a module from the sidebar.")
+    st.image("homepage.png", use_column_width=True)
