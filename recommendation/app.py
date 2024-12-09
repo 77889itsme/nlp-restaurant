3fc9ec59548fcdfdd22cb1d3f697ab4f80543444
your name goes here <133010759+77889itsme@users.noreply.github.com>
2024-12-08 21:10:29 -0500

    Update app.py

--- recommendation/app.py
+++ recommendation/app.py
@@ -6,15 +6,16 @@ def run_recommendation(df):
     st.header("Restaurant Recommendation System")
     
     user_input = st.text_area("Enter your review:")
-    user_city = st.text_input("Enter your city:")
+    user_city = st.text_input("Enter your city:", value="Santa Barbara")
+    st.caption("Currently, our app is limited to searches within Santa Barbara.")
     
     if st.button("Find Restaurants"):
         if user_input and user_city:
             recommendations = recommendation(df, user_input, user_city, top_n=5)
             if recommendations:
                 st.write("Top Recommendations:")
-                for rec in recommendations:
-                    st.subheader(rec['restaurant'])
+                for idx, rec in recommendations:
+                    st.subheader(f"{idx}. {rec['restaurant']}")
                     st.write(f"City: {rec['city']}")
                     st.write(f"Address: {rec['address']}")
                     st.write(f"Stars: {rec['stars']}")
